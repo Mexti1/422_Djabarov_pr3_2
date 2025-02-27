0b291cd7cadb4201b7e09872eda75a06a618108e
Mekhty <Mekhty@DESKTOP-L3V0H6K>
2025-02-27 23:57:18 +0300

    Добавьте файлы проекта.

---
+++ PR_3/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows;
+
+namespace PR_3
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CalculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                double amount = double.Parse(AmountTextBox.Text);
+                int months = int.Parse(MonthsTextBox.Text);
+                double rate = double.Parse(RateTextBox.Text) / 100;
+
+                double result;
+
+                if (SimpleInterestRadioButton.IsChecked == true)
+                {
+                    result = amount + (amount * rate * months / 12);
+                }
+                else
+                {
+                    result = amount * Math.Pow(1 + rate / 12, months);
+                }
+
+                ResultLabel.Content = $"Доход по вкладу = {result:F2}";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка ввода данных: " + ex.Message);
+            }
+        }
+    }
+}
